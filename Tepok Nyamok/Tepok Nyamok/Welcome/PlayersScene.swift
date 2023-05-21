2a178584f5a36f1ae53df0f0f80097a56db9a971
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-05-21 18:20:41 +0700

    3 Welcome pages done but text field still not implemented correctly

---
+++ Tepok Nyamok/Tepok Nyamok/Welcome/PlayersScene.swift
@@ -0,0 +1,13 @@
+//
+//  PlayersScene.swift
+//  Tepok Nyamok
+//
+//  Created by Leonardo Wijaya on 21/05/23.
+//
+
+import UIKit
+import SpriteKit
+
+class PlayerScene: SKScene {
+    
+}
---
+++ Tepok Nyamok/Tepok NyamokUITests/Tepok_NyamokUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tepok_NyamokUITestsLaunchTests.swift
+//  Tepok NyamokUITests
+//
+//  Created by Leonardo Wijaya on 19/05/23.
+//
+
+import XCTest
+
+final class Tepok_NyamokUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
